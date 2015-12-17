c4959cd1e3d9f6cb720e58a5365e94a54628695d
Pink Floyd <k.chenmingtong@gmail.com>
2015-12-17 16:53:33 +0800

    Signed-off-by: Pink Floyd <k.chenmingtong@gmail.com>

--- test.c
+++ test.c
@@ -4,9 +4,7 @@
 
 int main( int argc, char *argv[] ) {
     printf( 1, "Start Test!\n" );
-    printf( 3, "This process PID is ", getpid( ), "\nThen I will fork this process!\n" );
     sleep(3);
-    int childPID = fork();
-    printf( 3, "\nThen " ,childPID, "Child will kill father" ,kill(getpid()));
+    fork();
     exit( );
 }
